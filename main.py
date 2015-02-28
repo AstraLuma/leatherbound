48aec674e21c8919db9b2fca0a4ebba521329e02
Jamie Bliss <astronouth7303@gmail.com>
2015-02-28 09:33:20 -0500

    Refactor into package

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-app.config['DEBUG'] = True
+from leatherbound import app
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
-
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    """Return a custom 404 error."""
-    return 'Sorry, nothing at this URL.', 404
+app
